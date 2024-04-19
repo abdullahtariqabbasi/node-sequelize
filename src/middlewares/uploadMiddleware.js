a83245ef341c5e597807b6289a79f7954982f3bf
Abdullah Tariq Abbasi <abdullahabbasi852@gmail.com>
2024-04-19 17:56:05 +0500

    Removed comments

--- src/middlewares/uploadMiddleware.js
+++ src/middlewares/uploadMiddleware.js
@@ -2,10 +2,10 @@ import multer, { diskStorage } from 'multer';
 
 const storage = diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Specify the directory where uploaded files will be stored
+    cb(null, 'uploads/');
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname); // Use the original file name for storing
+    cb(null, file.originalname);
   }
 });
 
